f18fc4f2d8015f5c750477fbc4a58010e5f32d14
VLovchikov <vlad-0901@yandex.ru>
2018-05-10 15:35:21 +0300

    TEST
    Add about 3 tests

--- Text/UnitTest/TestStack.cpp
+++ Text/UnitTest/TestStack.cpp
@@ -6,3 +6,15 @@ TEST(Stack, can_create_stack)
 {
 	ASSERT_NO_THROW(Stack ss);
 }
+
+TEST(Stack, empty_stack_is_empty)
+{
+	Stack ss;
+	EXPECT_EQ(true,ss.isEmpty());
+}
+
+TEST(Stack, cannot_pop_if_stack_is_empty)
+{
+	Stack ss;
+	ASSERT_ANY_THROW(ss.pop());
+}
